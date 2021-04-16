c0d0cff7955dc0a279812d594c9c58880e19a580
tekeshwar810 <tekeshwar810@gmail.com>
2021-04-16 10:10:44 +0530

    ap16,10:10

--- mymember_B/routes/std_temp_list.js
+++ mymember_B/routes/std_temp_list.js
@@ -5,5 +5,6 @@ const {tempList_create,Email_contact_list } = require("../controllers/std_temp_l
 
 router.get('/email_compose/student_email_temp_list/:userId',verifySchool,Email_contact_list)
 router.post('/create_contact_email_list/:userId',verifySchool,tempList_create)
+//fdsf
 
 module.exports = router
