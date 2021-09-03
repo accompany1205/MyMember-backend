f27bdf88159af8c887efda110a31c0f4a8dc4d34
Monust0036 <monusveltosest0036@gmail.com>
2021-09-03 23:42:21 +0530

    added nurturing section

--- controllers/nurturing_template.js
+++ controllers/nurturing_template.js
@@ -1,5 +1,5 @@
 const template = require("../models/emailSentSave")
-const nurturing_folder = require("../models/email_nurturing_folder")
+const nurturingFolderModal = require("../models/email_nurturing_folder")
 const key = require("../models/email_key")
 
 function timefun(sd,st){
@@ -22,7 +22,7 @@ function timefun(sd,st){
 }
 
 exports.list_template = (req,res)=>{
-    nurturing_folder.findById(req.params.folderId)
+    nurturingFolderModal.findById(req.params.folderId)
     .populate('template')
     .exec((err,template_data)=>{
         if(err){
@@ -35,83 +35,162 @@ exports.list_template = (req,res)=>{
     })
 }
 
-exports.add_template = (req,res)=>{
+exports.add_template = async(req,res)=>{
+    // var schedule = req.body.schedule
+    //    authKey.findOne({userId:req.params.userId})      
+    //     .exec((err,key)=>{
+    //         if(err){
+    //             res.send({Error:'email auth key is not find so schedule is not create',error:err})
+    //         }
+    //         else{
+    //             console.log(key)
+
+            let { to, from, title, subject, template, sent_time, repeat_mail, follow_up } = req.body || {};
+            let { userId, folderId } = req.params || {};
+
+            const obj = {
+                to,
+                from,
+                title,
+                subject, 
+                template,
+                sent_date: nD,
+                sent_time,
+                DateT:date_iso_follow,
+                repeat_mail,
+                follow_up,
+                email_type:'schedule',
+                email_status:true,
+                category:'nurturing',
+                userId,
+                folderId
+            };
+
+            console.log(obj);
             if(req.body.follow_up === 0){
                 var date_iso = timefun(req.body.sent_date,req.body.sent_time)
-                console.log(date_iso)
-              
-                          var obj = {
-                              to: req.body.to,
-                              from: req.body.from,
-                              title:req.body.title,
-                              subject:req.body.subject, 
-                              template:req.body.template,
-                              sent_date: req.body.sent_date,
-                              sent_time: req.body.sent_time,
-                              DateT:date_iso,
-                              repeat_mail: req.body.repeat_mail,
-                              follow_up: req.body.follow_up,
-                              email_type:'schedule',
-                              email_status:true,
-                              category:'nurturing',
-                              userId:req.params.userId,
-                              folderId:req.params.folderId
-                          }
-                      }
+                obj.DateT = date_iso;
+            }
+            else if(req.body.follow_up > 0){
+                var date_iso_follow = timefun(req.body.sent_date,req.body.sent_time)
+                date_iso_follow.setDate(date_iso_follow.getDate() + req.body.follow_up);
+                var nD = moment(date_iso_follow).format('MM/DD/YYYY')    
+                // console.log(nD,'lo')// this is date mm/dd/yyyy
+                // console.log(date_iso_follow); // this is iso date time
+                // console.log(date_iso_follow.getHours(),'hour')
+            }
+            else if(req.body.follow_up < 0){
+                res.send({code:400,msg:'follow up not set less then 0'})
+            }
 
-                      else if(req.body.follow_up > 0){
-                        var date_iso_follow = timefun(req.body.sent_date,req.body.sent_time)
-        
-                        console.log(date_iso_follow,'si')
-                        date_iso_follow.setDate(date_iso_follow.getDate() + req.body.follow_up);
-                        var mdy = moment(date_iso_follow).format('MM/DD/YYYY')    
-                        console.log(mdy,'lo')// this is date mm/dd/yyyy
-                        console.log(date_iso_follow); // this is iso date time
-                        console.log(date_iso_follow.getHours(),'hrou')
 
-                        var obj = {
-                            to: req.body.to,
-                            from: req.body.from,
-                            title:req.body.title,
-                            subject:req.body.subject, 
-                            template:req.body.template,
-                            sent_date: mdy,
-                            sent_time: req.body.sent_time,
-                            DateT:date_iso_follow,
-                            repeat_mail: req.body.repeat_mail,
-                            follow_up: req.body.follow_up,
-                            email_type:'schedule',
-                            email_status:true,
-                            category:'nurturing',
-                            userId:req.params.userId,
-                            folderId:req.params.folderId
-                        }
-                    }
-                    else if(req.body.follow_up < 0){
-                        res.send({code:400,msg:'follow up not set less then 0'})
-                    } 
+            var emailDetail =  new all_temp(obj)
+            console.log(emailDetail);
 
-                var emailDetail =  new template(obj)
-                console.log(emailDetail)
-                emailDetail.save((err,emailSave)=>{
-           if(err){
-              res.send({code:200,msg:'template not save'})
-              console.log(err)
-          }
-          else{
-            nurturing_folder.findByIdAndUpdate(req.params.folderId,{$push:{template:emailSave._id}})
-            .exec((err,template)=>{
-                if(err){
-                    res.send({code:400,msg:'nurturing template details is not add in folder'})
+
+            try {
+                let emailSave = await emailDetail.save();
+                let template = await nurturingFolderModal.findByIdAndUpdate(folderId,{$push:{template:emailSave._id}})
+                try {
+
+                    // wrong model
+                    // let allData = await compose_folder
+                    // .find({})
+                    // .populate('template');
+
+                    // right model
+                    let allData = await all_temp
+                    .find({})
+                    return res.send({msg:'compose template details is add in folder',result: allData});
                 }
-                else{
-                    res.send({code:200,msg:'nurturing template details is add in folder',result:emailSave})
+                catch(err) {
+                    return res.send({error:'compose template details is not add in folder'})
                 }
-            })
-          }
-      })
-   
+            }
+
+            catch(err) {
+                console.log(err);
+                res.send({Error:'email details is not save',error:err})
+            }
 }
+
+// exports.add_template = (req,res)=>{
+//             if(req.body.follow_up === 0){
+//                 var date_iso = timefun(req.body.sent_date,req.body.sent_time)
+//                 console.log(date_iso)
+              
+//                           var obj = {
+//                               to: req.body.to,
+//                               from: req.body.from,
+//                               title:req.body.title,
+//                               subject:req.body.subject, 
+//                               template:req.body.template,
+//                               sent_date: req.body.sent_date,
+//                               sent_time: req.body.sent_time,
+//                               DateT:date_iso,
+//                               repeat_mail: req.body.repeat_mail,
+//                               follow_up: req.body.follow_up,
+//                               email_type:'schedule',
+//                               email_status:true,
+//                               category:'nurturing',
+//                               userId:req.params.userId,
+//                               folderId:req.params.folderId
+//                           }
+//                       }
+
+//                       else if(req.body.follow_up > 0){
+//                         var date_iso_follow = timefun(req.body.sent_date,req.body.sent_time)
+        
+//                         console.log(date_iso_follow,'si')
+//                         date_iso_follow.setDate(date_iso_follow.getDate() + req.body.follow_up);
+//                         var mdy = moment(date_iso_follow).format('MM/DD/YYYY')    
+//                         console.log(mdy,'lo')// this is date mm/dd/yyyy
+//                         console.log(date_iso_follow); // this is iso date time
+//                         console.log(date_iso_follow.getHours(),'hrou')
+
+//                         var obj = {
+//                             to: req.body.to,
+//                             from: req.body.from,
+//                             title:req.body.title,
+//                             subject:req.body.subject, 
+//                             template:req.body.template,
+//                             sent_date: mdy,
+//                             sent_time: req.body.sent_time,
+//                             DateT:date_iso_follow,
+//                             repeat_mail: req.body.repeat_mail,
+//                             follow_up: req.body.follow_up,
+//                             email_type:'schedule',
+//                             email_status:true,
+//                             category:'nurturing',
+//                             userId:req.params.userId,
+//                             folderId:req.params.folderId
+//                         }
+//                     }
+//                     else if(req.body.follow_up < 0){
+//                         res.send({code:400,msg:'follow up not set less then 0'})
+//                     } 
+
+//                 var emailDetail =  new template(obj)
+//                 console.log(emailDetail)
+//                 emailDetail.save((err,emailSave)=>{
+//            if(err){
+//               res.send({code:200,msg:'template not save'})
+//               console.log(err)
+//           }
+//           else{
+//             nurturing_folder.findByIdAndUpdate(req.params.folderId,{$push:{template:emailSave._id}})
+//             .exec((err,template)=>{
+//                 if(err){
+//                     res.send({code:400,msg:'nurturing template details is not add in folder'})
+//                 }
+//                 else{
+//                     res.send({code:200,msg:'nurturing template details is add in folder',result:emailSave})
+//                 }
+//             })
+//           }
+//       })
+   
+// }
 
 exports.remove_template =(req,res)=>{
     template.findByIdAndRemove(req.params.templateId,(err,removeTemplate)=>{
@@ -120,7 +199,7 @@ exports.remove_template =(req,res)=>{
         }
         else{
             console.log(removeTemplate)
-            nurturing_folder.update({"template":removeTemplate._id},{$pull:{"template":removeTemplate._id}},
+            nurturingFolderModal.update({"template":removeTemplate._id},{$pull:{"template":removeTemplate._id}},
             function(err,temp){
                 if(err){
                     res.send({error:'nurturing template details is not remove in folder'})
@@ -129,6 +208,18 @@ exports.remove_template =(req,res)=>{
                     res.send({msg:'nurturing template is remove successfully'})
                 }
             })
+        }
+    })
+}
+
+exports.all_email_list = async(req,res)=>{
+    template.find({userId:req.params.userId})
+    .exec((err,allTemp)=>{
+        if(err){
+            res.send({code:400,msg:'email list not found'})
+        }
+        else{
+            res.send({code:200,msg:allTemp})
         }
     })
 }
