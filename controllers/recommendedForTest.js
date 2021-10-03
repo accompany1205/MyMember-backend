e6dfb906bece7b10d0e505792aa9048155e95324
Monust0036 <monusveltosest0036@gmail.com>
2021-10-03 22:54:46 +0530

    updated recommendeForTest post api

--- controllers/recommendedForTest.js
+++ controllers/recommendedForTest.js
@@ -34,12 +34,14 @@ exports.getRecommededForTest = async (req, res) => {
 exports.recomendStudent = async (req, res) => {
     //only accepte array of objects
     let students = req.body;
+    let userId = req.params.userId;
     if (!students) {
         res.json({
             status: false,
             msg: "You haven't selected any student!"
         })
     }
+
     const recommendedStudentsForTest = [];
     for (let student of students) {
         let {
@@ -49,8 +51,14 @@ exports.recomendStudent = async (req, res) => {
             rating,
             current_rank_name,
             next_rank_name,
-            userId
+            memberprofileImage,
+            phone,
+            program,
+            method,
+            status,
+            lastPromotedDate
         } = student;
+        console.log("student-->", next_rank_name)
         let isStudentExists = await RecommendedForTest.find({
             "studentId": studentId
         });
@@ -62,12 +70,20 @@ exports.recomendStudent = async (req, res) => {
                 "rating": rating,
                 "current_rank_name": current_rank_name,
                 "next_rank_name": next_rank_name,
-                "userId": userId
+                "userId": userId,
+                "memberprofileImage":memberprofileImage,
+                "phone":phone,
+                "program":program,
+                "lastPromotedDate":lastPromotedDate,
+                "method":method,
+                "status":status
             })
         }
     }
+    //console.log("recommendedStudentsForTest", recommendedStudentsForTest)
 
     let recommended = await RecommendedForTest.insertMany(recommendedStudentsForTest);
+    //console.log("recommended->",recommended)
     if (!recommended.length) {
         res.json({
             status: false,
