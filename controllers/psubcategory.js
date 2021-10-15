c854796430c8df4d2ba555a4f00b1be4ff4c73da
Monust0036 <monusveltosest0036@gmail.com>
2021-10-15 21:56:40 +0530

    added account number to school

--- controllers/psubcategory.js
+++ controllers/psubcategory.js
@@ -5,17 +5,9 @@ const { deleteOne, $where } = require("../models/psubcategory");
 exports.create = (req, res) => {
     console.log(req.body)
     var categoryId = req.params.catId;
-    var category = req.body.category;
-    var subcategoryDetails = category.subcatdetails;
-
- 
-        obj = {
-            subcategory: row.subcategoryname,
-            color: row.color,
-            lable: row.lable,
-            category: category.name
-        }
-        var psubcategoryObj = new psubcategory(obj)
+    var subcategory = req.body.category;
+    
+        var psubcategoryObj = new psubcategory(subcategory)
         console.log(psubcategoryObj)
         psubcategoryObj.save((err, data) => {
             if (err) {
