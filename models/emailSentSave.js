f12bb40bc833f7d607e749967310dbbb60bff19a
parmeshwar <rathodparmeshwar4321@gmail.com>
2021-10-23 16:24:56 +0530

    dragAndDrop api added + isFavoritekey added in model

--- models/emailSentSave.js
+++ models/emailSentSave.js
@@ -1,72 +1,79 @@
 const { any } = require('async');
 const mongoose = require('mongoose');
-const schema =  mongoose.Schema
+const schema = mongoose.Schema
 const EmailSchema = schema({
-   from:{
-       type:String,
-       require:true
-   },
-   to:{
-       type:Array,
-       require:true
-   },
-   title:{
-       type:String,
-       require:true
-   },
-   subject:{
-        type:String,
-        require:true
-   }, 
-   template:{
-        type:String
-   },
-   repeat_mail:{
-        type:String
-   },
-   follow_up:{
-       type:String
-   },
-   sent_date:{
-        type:String,
-   },
-   sent_time:{
-        type:String,
-   },
-   DateT:{
-       type:Date
-   },
-   category:{
-       type:String,
-       default:' '
-   },
-   email_type:{
-       type:String,
-       default:' ' 
-   },
-   email_status:{
-       type:Boolean,
-   },
-   email_auth_key:{
-       type:String,
-       require:true 
-   },
-   userId:{
-       type:String,
-       require:true 
-   },
-   folderId:{
-        type:String,
-        require:true
-   },
-   createdBy:{
-        type:String
-   },
-   adminId:{
-        type:String
-   }
+    from: {
+        type: String,
+        require: true
+    },
+    to: {
+        type: Array,
+        require: true
+    },
+    title: {
+        type: String,
+        require: true
+    },
+    subject: {
+        type: String,
+        require: true
+    },
+    template: {
+        type: String
+    },
+    repeat_mail: {
+        type: String
+    },
+    follow_up: {
+        type: String
+    },
+    sent_date: {
+        type: String,
+    },
+    sent_time: {
+        type: String,
+    },
+    DateT: {
+        type: Date
+    },
+    category: {
+        type: String,
+        default: ' '
+    },
+    email_type: {
+        type: String,
+        default: ' '
+    },
+    email_status: {
+        type: Boolean,
+    },
+    email_auth_key: {
+        type: String,
+        require: true
+    },
+    userId: {
+        type: String,
+        require: true
+    },
+    folderId: {
+        type: String,
+        require: true
+    },
+    createdBy: {
+        type: String
+    },
+    adminId: {
+        type: String
+    },
+    templete_Id: {
+        type: String
+    },
+    is_Favorite: {
+        type: Boolean,
+        default:false
+    }
 },
-{ timestamps:true }
+    { timestamps: true }
 )
 
 module.exports = mongoose.model('sentOrscheduleEmail', EmailSchema)
