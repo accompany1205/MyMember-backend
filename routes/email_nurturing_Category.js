7383b08d75c1fd38f6aee82a8aae660163ddbed2
Monust0036 <monusveltosest0036@gmail.com>
2021-11-22 13:25:50 +0530

    Multer changes

--- routes/email_nurturing_Category.js
+++ routes/email_nurturing_Category.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const upload = require('../handler/multer');
 const { requireSignin, isAuth, verifySchool } = require("../controllers/auth");
 const { category_list, addcategory, updateCategory, removeCategory, tempList, sendEmail, userEmailList } = require("../controllers/email_nurturing_Category")
 
 router.post("/email_nurturing/send_email/:userId", verifySchool, sendEmail);
 
-router.get("/email_nurturing/category_list/:userId", verifySchool, category_list)
+router.get("/email_nurturing/category_list/:userId", verifySchool,upload.array('attachments'), category_list)
 router.post("/email_nurturing/addcategory/:userId", verifySchool, addcategory)
 router.put("/email_nurturing/edit_category/:userId/:categoryId", requireSignin, updateCategory)
 router.delete("/email_nurturing/remove_category/:userId/:categoryId", requireSignin, removeCategory)
