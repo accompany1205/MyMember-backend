a8f83ffbe384e16b863512a4f53d697af6f486dd
Monust0036 <monusveltosest0036@gmail.com>
2021-12-20 04:58:02 +0530

    route name changes

--- routes/appointment_event.js
+++ routes/appointment_event.js
@@ -5,7 +5,7 @@ const { verifySchool } = require("../controllers/auth");
 
 
 router.post("/create_appointmentevent/:userId", verifySchool, create);
-router.put("/update_appointmentevent/:userId/:docId", verifySchool,update);
+router.put("/update_appointmentcategory/:userId/:docId", verifySchool,update);
 router.get("/list_of_appointmentcategory/:userId", verifySchool, getAllEvents);
-router.delete("/delete_appointmentevent/:userId/:docId", verifySchool, deleteEvent)
+router.delete("/delete_appointmentcategory/:userId/:docId", verifySchool, deleteEvent)
 module.exports = router;
