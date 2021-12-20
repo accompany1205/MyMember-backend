a1d81df23eb74a1d73b19438837273396f3dd791
parmeshwar <rathodparmeshwar4321@gmail.com>
2021-12-21 02:11:03 +0530

    channges in smartlist logic

--- controllers/smartlists.js
+++ controllers/smartlists.js
@@ -27,124 +27,78 @@ exports.create_smart_list = async (req, res) => {
             })
         }
         let promises = [];
+        let { leads, activetrial, formertrial, activestudent, formerstudent, status, programName, current_rank_name, age, zipPostalCode, location } = req.body
 
         if (req.body.leads) {
-            let leadData = await member.find({ studentType: req.body.leads, userId: userId }, { email: 1 })
+            let leadData = await member.find({
+                studentType: leads,
+                status: status,
+                program: programName,
+                current_rank_name: current_rank_name,
+                age: age,
+                location: location,
+                zipPostalCode: zipPostalCode,
+                userId: userId
+            }, { email: 1 })
             promises = [...leadData];
-            if (req.body.programName) {
-                console.log(req.body.programName)
-                let programData = await member.find({ program: req.body.programName, userId: userId }, { email: 1 })
-                promises = [...programData, ...promises];
-            }
-            if (req.body.current_rank_name) {
-                console.log(req.body.current_rank_name)
-                let rankData = await member.find({ current_rank_name: req.body.current_rank_name, userId: userId }, { email: 1 })
-                promises = [...rankData, ...promises];
-            }
-            if (req.body.age) {
-                console.log(req.body.age)
-                let ageData = await member.find({ age: req.body.age, userId: userId }, { email: 1 })
-                promises = [...ageData, ...promises];
-            }
-            if (req.body.location) {
-                console.log(req.body.location)
-                let locationData = await member.find({ location: req.body.location, userId: userId }, { email: 1 })
-                promises = [...locationData, ...promises];
-            }
         }
         if (req.body.activetrial) {
-            let activeTData = await member.find({ studentType: req.body.activetrial, userId: userId }, { email: 1 })
+            let activeTData = await member.find({
+                studentType: activetrial,
+                status: status,
+                program: programName,
+                current_rank_name: current_rank_name,
+                age: age,
+                location: location,
+                zipPostalCode: zipPostalCode,
+                userId: userId
+            }, { email: 1 })
             promises = [...activeTData, ...promises]
-            if (req.body.programName) {
-                let programData = await member.find({ program: req.body.programName, userId: userId }, { email: 1 })
-                promises = [...programData, ...promises];
-            }
-            if (req.body.current_rank_name) {
-                console.log(req.body.current_rank_name)
-                let rankData = await member.find({ current_rank_name: req.body.current_rank_name, userId: userId }, { email: 1 })
-                promises = [...rankData, ...promises];
-            }
-            if (req.body.age) {
-                console.log(req.body.age)
-                let ageData = await member.find({ age: req.body.age, userId: userId }, { email: 1 })
-                promises = [...ageData, ...promises];
-            }
-            if (req.body.location) {
-                console.log(req.body.location)
-                let locationData = await member.find({ location: req.body.location, userId: userId }, { email: 1 })
-                promises = [...locationData, ...promises];
-            }
         }
         if (req.body.formertrial) {
-            let formerTData = await member.find({ studentType: req.body.formertrial, userId: userId }, { email: 1 })
+            let formerTData = await member.find({
+                studentType: formertrial,
+                status: status,
+                program: programName,
+                current_rank_name: current_rank_name,
+                age: age,
+                location: location,
+                zipPostalCode: zipPostalCode,
+                userId: userId
+            }, { email: 1 })
             promises = [...formerTData, ...promises]
-            if (req.body.programName) {
-                let programData = await member.find({ program: req.body.programName, userId: userId }, { email: 1 })
-                promises = [...programData, ...promises];
-            }
-            if (req.body.current_rank_name) {
-                console.log(req.body.current_rank_name)
-                let rankData = await member.find({ current_rank_name: req.body.current_rank_name, userId: userId }, { email: 1 })
-                promises = [...rankData, ...promises];
-            }
-            if (req.body.age) {
-                console.log(req.body.age)
-                let ageData = await member.find({ age: req.body.age, userId: userId }, { email: 1 })
-                promises = [...ageData, ...promises];
-            }
-            if (req.body.location) {
-                console.log(req.body.location)
-                let locationData = await member.find({ location: req.body.location, userId: userId }, { email: 1 })
-                promises = [...locationData, ...promises];
-            }
+
         }
         if (req.body.activestudent) {
-            let activeSData = await member.find({ studentType: req.body.activestudent, userId: userId }, { email: 1 })
+            let activeSData = await member.find({
+                studentType: activestudent,
+                status: status,
+                program: programName,
+                current_rank_name: current_rank_name,
+                age: age,
+                location: location,
+                zipPostalCode: zipPostalCode,
+                userId: userId
+            }, { email: 1 })
             promises = [...activeSData, ...promises]
-            if (req.body.programName) {
-                let programData = await member.find({ program: req.body.programName, userId: userId }, { email: 1 })
-                promises = [...programData, ...promises];
-            }
-            if (req.body.current_rank_name) {
-                console.log(req.body.current_rank_name)
-                let rankData = await member.find({ current_rank_name: req.body.current_rank_name, userId: userId }, { email: 1 })
-                promises = [...rankData, ...promises];
-            }
-            if (req.body.age) {
-                console.log(req.body.age)
-                let ageData = await member.find({ age: req.body.age, userId: userId }, { email: 1 })
-                promises = [...ageData, ...promises];
-            }
-            if (req.body.location) {
-                console.log(req.body.location)
-                let locationData = await member.find({ location: req.body.location, userId: userId }, { email: 1 })
-                promises = [...locationData, ...promises];
-            }
+
         }
         if (req.body.formerstudent) {
-            let formerSData = await member.find({ studentType: req.body.formerstudent, userId: userId }, { email: 1 })
+            let formerSData = await member.find({
+                studentType: formerstudent,
+                status: status,
+                program: programName,
+                current_rank_name: current_rank_name,
+                age: age,
+                location: location,
+                zipPostalCode: zipPostalCode,
+                userId: userId
+            }, { email: 1 })
             promises = [...formerSData, ...promises]
-            if (req.body.programName) {
-                let programData = await member.find({ program: req.body.programName, userId: userId }, { email: 1 })
-                promises = [...programData, ...promises];
-            }
-            if (req.body.current_rank_name) {
-                console.log(req.body.current_rank_name)
-                let rankData = await member.find({ current_rank_name: req.body.current_rank_name, userId: userId }, { email: 1 })
-                promises = [...rankData, ...promises];
-            }
-            if (req.body.age) {
-                console.log(req.body.age)
-                let ageData = await member.find({ age: req.body.age, userId: userId }, { email: 1 })
-                promises = [...ageData, ...promises];
-            }
-            if (req.body.location) {
-                console.log(req.body.location)
-                let locationData = await member.find({ location: req.body.location, userId: userId }, { email: 1 })
-                promises = [...locationData, ...promises];
-            }
+
         }
         await Promise.all(promises);
+        console.log(promises.length)
         const map = {};
         const newArray = [];
         promises.forEach(el => {
