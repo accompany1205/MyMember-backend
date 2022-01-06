177c8c2d85a1caac6aceef43a48dc98686390c3b
parmeshwar <rathodparmeshwar4321@gmail.com>
2022-01-06 16:26:02 +0530

    changes in admin product  updated multer handler

--- routes/admin/product_manage/admin_product.js
+++ routes/admin/product_manage/admin_product.js
@@ -6,9 +6,9 @@ const { create, read, product_info, deleteproduct, updateproduct, } = require('.
 // const { assign_product } = require('../../../controllers/admin/product_management/admin_product')
 router.get('/admin/product/product_list/:adminId', isAdmin, read)
 router.get('/admin/product/info_product/:adminId/:productId', isAdmin, product_info)
-router.post('/admin/product/add_product/:adminId/:folderId', upload.single('attach'), isAdmin, create)
+router.post('/admin/product/add_product/:adminId/:folderId', upload.array('attach'), isAdmin, create)
 // router.post('/admin/product/assign_product/:adminId/:userId/:folderId', isAdmin, create)
 router.delete('/admin/product/delete_product/:adminId/:productId', isAdmin, deleteproduct)
-router.put('/admin/product/update_product/:adminId/:productId', upload.single('attach'), isAdmin, updateproduct)
+router.put('/admin/product/update_product/:adminId/:productId', upload.array('attach'), isAdmin, updateproduct)
 
 module.exports = router;    
