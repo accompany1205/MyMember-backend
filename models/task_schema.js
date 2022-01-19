d1002370b8cd74d200e2d76ca728cd92ee5b4746
amirnajafi <amir.najafi66@gmail.com>
2022-01-19 03:34:10 +0300

    add task new model

---
+++ models/task_schema.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const schema = mongoose.Schema;
+const mongoosePaginate = require("mongoose-paginate-v2");
+
+const taskSchema = schema(
+    {
+        type: {
+            type: String,
+            trim: true,
+            required: true,
+            maxlength: 32
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: true,
+            maxlength: 32
+        },
+        label: {
+            type: String,
+        },
+        due_date: {
+            type: Date,
+        },
+        priority: {
+            type: Number,
+        },
+        status: {
+            type: Number,
+            required: true
+        },
+        description: {
+            type: String,
+        },
+        userId: {
+            type: String,
+        }
+    },
+    { timestamps: true }
+);
+taskSchema.plugin(mongoosePaginate);
+
+module.exports = mongoose.model("Tasks", taskSchema);
