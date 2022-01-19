378ef2f2c4aedf5959bc1d512f39fc9fef6e5e39
amirnajafi <amir.najafi66@gmail.com>
2022-01-20 02:50:51 +0300

    add goals apis

--- models/goal_schema.js
+++ models/goal_schema.js
@@ -1,38 +1,65 @@
 const mongoose = require("mongoose");
+const mongoosePaginate = require("mongoose-paginate-v2");
 
-const todoSchema = new mongoose.Schema(
-    {
-        subject: {
+// const todoSchema = new mongoose.Schema(
+//     {
+//         subject: {
+//             type: String,
+//             trim: true,
+//             required: true,
+//             maxlength: 32
+//         },
+       
+//         goal_category: {type: String, required: true},
+
+//         compeleting_Date: {
+//             type: String,
+//             required: true
+//         },
+//          reminder_Date: {
+//             type: String,
+//             required: true
+//         }, 
+//         tag: {
+//             type: String,
+//             required: true
+//         },
+//          goal_status: {
+//             type: String,
+//             required: true
+//         },
+//         notes:{ type: String, required: true },
+//         userId:{
+//             type:String
+//         }
+//     },
+//     { timestamps: true }
+// );
+const goalSchema = new mongoose.Schema(
+        {
+        type: {
+            type: String,
+            trim: true,
+            required: true,
+            maxlength: 32,
+            enum : ['personal','mymember'],
+        },
+        name: {
             type: String,
             trim: true,
             required: true,
             maxlength: 32
         },
-       
-        goal_category: {type: String, required: true},
-
-        compeleting_Date: {
+        icon: {
             type: String,
-            required: true
         },
-         reminder_Date: {
+        color : {
             type: String,
-            required: true
-        }, 
-        tag: {
-            type: String,
-            required: true
-        },
-         goal_status: {
-            type: String,
-            required: true
-        },
-        notes:{ type: String, required: true },
-        userId:{
-            type:String
         }
     },
     { timestamps: true }
 );
+goalSchema.plugin(mongoosePaginate);
 
-module.exports = mongoose.model("Goal", todoSchema);
+
+module.exports = mongoose.model("goals", goalSchema);
---
+++ routes/Gloals.js
@@ -0,0 +1,105 @@
+const express = require('express')
+const Router = express.Router();
+const fs = require('fs'); 
+const model = require("../models/goal_schema");
+const moment = require('moment');
+
+class Goals {
+    constructor() {
+        Router.get('/goals/:id?', this.Get);
+        Router.post('/goals', this.Post);
+        Router.put('/goals/:id', this.Put);
+        Router.delete('/goals/:id', this.delete);
+    }
+
+    // Get method
+    Get = async (req, res) => {
+        try {
+            const {id} = req.params
+            const input = req.query
+            let conditions = {}
+            if (id) {
+                model.findById(id, function (err, item) {
+                    return res.status(200).json({ message: "ok", data: item });
+                });
+            } else {
+                const { type = null, name = null, goal_type = null, priority = null, status = null , withStats = null , page = 1 , page_size  = 20 } = req.query;
+                if (type) {
+                    conditions.type = type;
+                }
+                if (name) {
+                    conditions.name = { $regex: name, $options: 'i' };
+                }
+                model.paginate(conditions , { page, limit: page_size }, async function (err, items) {
+                    if (err) {
+
+                        return res.status(400).json({ message: err.message });
+                    }
+                    return res.status(200).json({ message: "ok", data: items });
+                });
+            }
+
+        } catch (err) {
+            console.log("sdfds" , err)
+            return res.status(400).json({ message: err });
+        }
+    }
+
+    Post = async (req, res) => {
+        try {
+            const input = req.body
+            const item = new model(input);
+            item.save((err, data) => {
+                if (err) {
+                    return res.status(400).json({ message: err });
+                } else {
+                    return res.status(400).json({ message: "item add successfuly", data: data  }) ;
+                }
+            });
+        } catch (err) {
+
+        }
+    }
+
+    Put = async (req, res) => {
+        try {
+            const {id} = req.params
+            const input = req.body
+            if (id) {
+                model.findByIdAndUpdate(id, input , {} , (err , data)=>{
+                    if (err) {
+                        return res.status(400).json({ message: err });
+                    }
+                    return res.status(200).json({ message: "item updated successfuly"  , data });
+                })
+            }else{
+                return res.status(400).json({ message: "id is required" });
+            }
+        } catch (err) {
+            return res.status(500).json({ message: err });
+        }
+    }
+
+    delete = async (req, res) => {
+        try {
+            const {id} = req.params
+            const input = req.body
+            if (id) {
+                model.findOneAndDelete({_id : id}, {} , (err , data)=>{
+                    console.log(err , data)
+                    if (err) {
+                        return res.status(400).json({ message: err });
+                    }
+                    return res.status(200).json({ message: "item remove successfuly"  , data });
+                })
+            }else{
+                return res.status(400).json({ message: "id is required" });
+            }
+        } catch (err)  {
+
+        }
+    }
+
+}
+new Goals();
+module.exports = Router;
