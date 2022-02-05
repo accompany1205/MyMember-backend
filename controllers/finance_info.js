89f260974584dd4cf6c754f5fbca084f496cdd72
Alamgir Ahamed Saif <88821633+binarybunon@users.noreply.github.com>
2022-02-05 18:22:23 +0600

    finance report monthly,weekly,daily, yearly, states routes

--- controllers/finance_info.js
+++ controllers/finance_info.js
@@ -1,8 +1,10 @@
 const finance_info = require('../models/finance_info');
+const Expense = require('../models/expenses');
 const bcrypt = require('bcryptjs');
 const addmemberModal = require('../models/addmember');
 const _ = require('lodash');
-
+const mongoose = require('mongoose');
+const moment = require('moment');
 exports.create = async (req, res) => {
 	try {
 		const { studentId, userId } = req.params;
@@ -137,4 +139,257 @@ exports.fetchAllCC = async (req, res) => {
 		.limit(limit)
 		.lean();
 	res.json({ count, list });
+};
+
+exports.expenseStateByCategory = async (req, res) => {
+	// fetch expese state
+	const total = await Expense.aggregate([
+		{ $match: { userId: mongoose.Types.ObjectId(req.params.userId) } },
+		{
+			$group: {
+				_id: 'total',
+				total: { $sum: '$amount' },
+			},
+		},
+	]);
+	let totalExpense = 0;
+	if (total && total.length > 0) {
+		totalExpense = total[0].total;
+	}
+	let expenses = await Expense.aggregate([
+		{ $match: { userId: mongoose.Types.ObjectId(req.params.userId) } },
+		{
+			$group: {
+				_id: '$category',
+				amount: { $sum: '$amount' },
+			},
+		},
+	]);
+	expenses = expenses.map((x) => {
+		let percentage = parseFloat((x.amount / totalExpense) * 100).toFixed(2);
+		return {
+			...x,
+			percentage,
+		};
+	});
+	return res.json(expenses);
+};
+
+exports.expenseMonthlyCompare = async (req, res) => {
+	// Current Month
+	const currentDate = new Date();
+	const currentMonth = currentDate.getMonth() + 1;
+	const currentYear = currentDate.getFullYear();
+	// Previous Month
+	currentDate.setMonth(currentMonth - 1);
+	const previousMonth = currentDate.getMonth();
+	const previousYear = currentDate.getFullYear();
+
+	let expenses = await Expense.aggregate([
+		{ $match: { userId: mongoose.Types.ObjectId(req.params.userId) } },
+		{
+			$project: {
+				amount: 1,
+				category: 1,
+				month: { $month: '$date' },
+				year: { $year: '$date' },
+			},
+		},
+
+		{
+			$match: {
+				$or: [
+					{
+						$and: [{ month: currentMonth }, { year: currentYear }],
+					},
+					{
+						$and: [{ month: previousMonth }, { year: previousYear }],
+					},
+				],
+			},
+		},
+		{
+			$group: {
+				_id: {
+					month: '$month',
+					year: '$year',
+					category: '$category',
+				},
+				amount: { $sum: '$amount' },
+			},
+		},
+	]);
+
+	const categories = [...new Set(expenses.map((x) => x._id.category))];
+	const expenseData = categories.map((x) => {
+		let current = expenses.find(
+			(expense) =>
+				expense._id.month === currentMonth &&
+				expense._id.year === currentYear &&
+				x === expense._id.category
+		);
+
+		let previous = expenses.find(
+			(expense) =>
+				expense._id.month === previousMonth &&
+				expense._id.year === previousYear &&
+				x === expense._id.category
+		);
+
+		return {
+			category: x,
+			current: current ? current.amount : 0,
+			previous: previous ? previous.amount : 0,
+		};
+	});
+
+	return res.json(expenseData);
+};
+
+exports.todaysExpense = async (req, res) => {
+	// Current Month
+	const currentDate = new Date();
+	const currentMonth = currentDate.getMonth() + 1;
+	const currentDay = currentDate.getDate();
+	const currentYear = currentDate.getFullYear();
+
+	let expenseData = await Expense.aggregate([
+		{ $match: { userId: mongoose.Types.ObjectId(req.params.userId) } },
+		{
+			$project: {
+				amount: 1,
+				category: 1,
+				month: { $month: '$date' },
+				year: { $year: '$date' },
+				day: { $dayOfMonth: '$date' },
+			},
+		},
+		{ $match: { day: currentDay, month: currentMonth, year: currentYear } },
+		{
+			$group: {
+				_id: '-',
+				amount: { $sum: '$amount' },
+			},
+		},
+	]);
+	let expense = 0;
+	if (expenseData && expenseData.length > 0) {
+		expense = expenseData[0].amount;
+	}
+
+	return res.send(expense + '');
+};
+
+exports.weeklyExpense = async (req, res) => {
+	// Current Month
+	const currentDate = new Date();
+	const sevenDaysAgo = new Date(moment(currentDate).subtract(6, 'days'));
+	const _local_String_current = currentDate
+		.toLocaleDateString(`fr-CA`)
+		.split('/')
+		.join('-');
+	const _local_String_sevenDaysAgo = sevenDaysAgo
+		.toLocaleDateString(`fr-CA`)
+		.split('/')
+		.join('-');
+
+	const start = `${_local_String_sevenDaysAgo}T00:00:00.00Z`;
+	const end = `${_local_String_current}T23:59:59.999Z`;
+
+	let expenseData = await Expense.aggregate([
+		{
+			$match: {
+				userId: mongoose.Types.ObjectId(req.params.userId),
+				date: {
+					$gte: new Date(start),
+					$lt: new Date(end),
+				},
+			},
+		},
+		{
+			$group: {
+				_id: '-',
+				amount: { $sum: '$amount' },
+			},
+		},
+	]);
+
+	let expense = 0;
+	if (expenseData && expenseData.length > 0) {
+		expense = expenseData[0].amount;
+	}
+
+	return res.send(expense + '');
+};
+
+exports.MonthlyExpense = async (req, res) => {
+	// Current Month
+	const currentDate = new Date();
+	const _local_String_current = currentDate
+		.toLocaleDateString(`fr-CA`)
+		.split('/')
+		.join('-');
+
+	currentDate.setDate(1);
+	const firstDateOfMonth = new Date(currentDate);
+	const _local_String_firstDateofMonth = firstDateOfMonth
+		.toLocaleDateString(`fr-CA`)
+		.split('/')
+		.join('-');
+
+	const start = `${_local_String_firstDateofMonth}T00:00:00.00Z`;
+	const end = `${_local_String_current}T23:59:59.999Z`;
+
+	let expenseData = await Expense.aggregate([
+		{
+			$match: {
+				userId: mongoose.Types.ObjectId(req.params.userId),
+				date: {
+					$gte: new Date(start),
+					$lt: new Date(end),
+				},
+			},
+		},
+		{
+			$group: {
+				_id: '-',
+				amount: { $sum: '$amount' },
+			},
+		},
+	]);
+
+	let expense = 0;
+	if (expenseData && expenseData.length > 0) {
+		expense = expenseData[0].amount;
+	}
+
+	return res.send(expense + '');
+};
+
+exports.thisYearExpense = async (req, res) => {
+	const currentDate = new Date();
+	const currentYear = currentDate.getFullYear();
+
+	let expenseData = await Expense.aggregate([
+		{ $match: { userId: mongoose.Types.ObjectId(req.params.userId) } },
+		{
+			$project: {
+				amount: 1,
+				year: { $year: '$date' },
+			},
+		},
+		{ $match: { year: currentYear } },
+		{
+			$group: {
+				_id: '-',
+				amount: { $sum: '$amount' },
+			},
+		},
+	]);
+	let expense = 0;
+	if (expenseData && expenseData.length > 0) {
+		expense = expenseData[0].amount;
+	}
+
+	return res.send(expense + '');
 };
--- routes/finance.js
+++ routes/finance.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const router = express.Router();
-const { fetchAllCC } = require('../controllers/finance_info');
+const {
+	fetchAllCC,
+	expenseStateByCategory,
+	expenseMonthlyCompare,
+	todaysExpense,
+	weeklyExpense,
+	MonthlyExpense,
+	thisYearExpense,
+} = require('../controllers/finance_info');
 const { requireSignin, isAuth, verifySchool } = require('../controllers/auth');
 
 router.get('/finance/fetch-all-cc/:userId', requireSignin, fetchAllCC);
+router.get(
+	'/finance/expense-state/:userId',
+	requireSignin,
+	expenseStateByCategory
+);
+
+router.get(
+	'/finance/expense-monthly-compare/:userId',
+	requireSignin,
+	expenseMonthlyCompare
+);
+
+router.get('/finance/expense-today/:userId', requireSignin, todaysExpense);
+router.get('/finance/expense-weekly/:userId', requireSignin, weeklyExpense);
+router.get('/finance/expense-monthly/:userId', requireSignin, MonthlyExpense);
+router.get('/finance/expense-yearly/:userId', requireSignin, thisYearExpense);
 
 module.exports = router;
