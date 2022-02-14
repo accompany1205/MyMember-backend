52d7c75f9da3928d046c9ca1ccc4575fa5347426
parmeshwar <rathodparmeshwar4321@gmail.com>
2022-02-14 15:42:41 +0530

    updated middlwear for admin system Email

--- routes/admin/system_email/admin_system_category.js
+++ routes/admin/system_email/admin_system_category.js
@@ -4,10 +4,10 @@ const upload = require('../../../handler/multer');
 const { isAdmin,verifySchool } = require("../../../controllers/auth")
 const { addCategory, updateCategory, removeCategory, admin_category_list, sendEmail, tempList } = require("../../../controllers/email_system_Category");
 
-router.get("/admin/email_system/category_list/:adminId", verifySchool, admin_category_list)
-router.post("/admin/email_system/send_email/:adminId", verifySchool,upload.array('attachments'), sendEmail);
-router.post("/admin/email_system/addCategory/:adminId", verifySchool, addCategory);
-router.put("/admin//email_system/edit_category/:adminId/:categoryId", verifySchool, updateCategory);
-router.delete("/admin//email_system/remove_category/:adminId/:categoryId", verifySchool, removeCategory);
+router.get("/admin/email_system/category_list/:adminId", isAdmin, admin_category_list)
+router.post("/admin/email_system/send_email/:adminId", isAdmin,upload.array('attachments'), sendEmail);
+router.post("/admin/email_system/addCategory/:adminId", isAdmin, addCategory);
+router.put("/admin//email_system/edit_category/:adminId/:categoryId", isAdmin, updateCategory);
+router.delete("/admin//email_system/remove_category/:adminId/:categoryId", isAdmin, removeCategory);
 
 module.exports = router;
--- routes/admin/system_email/admin_system_template.js
+++ routes/admin/system_email/admin_system_template.js
@@ -5,14 +5,14 @@ const { isAdmin, verifySchool } = require("../../../controllers/auth")
 const { add_template, update_template, remove_template, status_update_template, single_temp_update_status, swapAndUpdate_template, multipal_temp_remove } = require("../../../controllers/email_system_template")
 
 
-router.post("/admin//email_system/add_template/:adminId/:folderId", verifySchool, upload.array('attachments'), add_template)
-router.put("/admin//email_system/update_template/:adminId/:templateId", verifySchool, upload.array('attachments'), update_template)
-router.put("/admin//email_system/drag_drop_templete/:adminId", verifySchool, swapAndUpdate_template) //dragAndDrop
+router.post("/admin//email_system/add_template/:adminId/:folderId", isAdmin, upload.array('attachments'), add_template)
+router.put("/admin//email_system/update_template/:adminId/:templateId", isAdmin, upload.array('attachments'), update_template)
+router.put("/admin//email_system/drag_drop_templete/:adminId", isAdmin, swapAndUpdate_template) //dragAndDrop
 router.put("/admin//email_system/update_template_status/:adminId/:folderId", isAdmin, status_update_template)
 router.put("/admin//email_system/single_template_status_change/:adminId/:tempId", isAdmin, single_temp_update_status)//single template status change
 
-router.delete("/admin//email_system/remove_template/:adminId/:templateId", verifySchool, remove_template)
-router.delete("/admin//email_system/multipal_remove_template/:adminId/:folderId", verifySchool, multipal_temp_remove)
+router.delete("/admin//email_system/remove_template/:adminId/:templateId", isAdmin, remove_template)
+router.delete("/admin//email_system/multipal_remove_template/:adminId/:folderId", isAdmin, multipal_temp_remove)
 
 
 module.exports = router;
