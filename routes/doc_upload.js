037a24472f75e1f8df75b22049235979cd404f88
parmeshwar <rathodparmeshwar4321@gmail.com>
2022-02-16 00:22:44 +0530

    changes updated in doc_upload section

--- routes/doc_upload.js
+++ routes/doc_upload.js
@@ -5,8 +5,8 @@ const { docupload, file_sample, groupList, updatedocupload, docremove } = requir
 const upload = require("../handler/multer")
 
 router.get("/group_student_list/:userId", verifySchool, groupList)
-
 router.get("/sample_file/:userId", verifySchool, file_sample)
+
 router.post("/upload_document/:userId/:folderId/:subFolderId", verifySchool, upload.single('document'), docupload)
 router.put("/update_upload_document/:userId/:docId", verifySchool, upload.single('document'), updatedocupload)
 router.delete("/remove_upload_document/:userId/:docId", verifySchool, docremove)
