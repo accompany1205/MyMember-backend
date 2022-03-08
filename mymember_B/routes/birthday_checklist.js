d70ab6fc958dce84b4cd772b9a6ae1df09c289f2
Alamgir Ahamed Saif <88821633+binarybunon@users.noreply.github.com>
2022-03-08 15:45:56 +0600

    statictics page image

--- mymember_B/routes/birthday_checklist.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { BirthChecklist,remove }  = require("../controllers/birthday_checklist")
-const { requireSignin,isAuth,verifySchool } = require("../controllers/auth");
-
-router.post("/birthday/birthday_checklist/:userId/:studentId",verifySchool,BirthChecklist)
-router.delete("/birthday/remove_birthday_checklist/:userId/:checkListId",requireSignin,remove)
-
-module.exports = router;
