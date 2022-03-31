ec47bdbddb6af501bf872a5f4994f9e98573eb60
diwakar17 <diwakar17@navgurukul.org>
2022-04-01 00:56:04 +0530

    changes for text alert box

--- controllers/auth.js
+++ controllers/auth.js
@@ -528,7 +528,6 @@ exports.signin = async (req, res) => {
 					if (data.isEmailverify) {
 						if (data.status == 'Active') {
 							let locationData = await User.find({ _id: data.locations }).populate('default_location')
-							console.log(locationData);
 							let default_locationData = await location.find({ _id: data.default_location });
 							if (isAccessLocations) {
 								let current_locationData = await User.findOne({ locationName: req.body.locationName });
