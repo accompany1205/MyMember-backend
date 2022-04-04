9dade59e9c5b02923931351408e60f7c202bf799
diwakar17 <diwakar17@navgurukul.org>
2022-04-04 22:41:44 +0530

    chnages for password

--- validator/index.js
+++ validator/index.js
@@ -6,12 +6,6 @@ exports.userSignupValidator = (req, res, next) => {
             min: 4,
             max: 52
         });
-    req.check('password')
-        .isLength({ min: 6 })
-        .withMessage('Password must contain at least 6 characters')
-        .matches(/\d/)
-        .withMessage('Password must contain a number');
-
     const errors = req.validationErrors();
     if (errors) {
         const firstError = errors.map(error => error.msg)[0];
