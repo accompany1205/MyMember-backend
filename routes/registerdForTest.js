3962028f8c98cb4f252fa657211db4e1833bb512
parmeshwar <rathodparmeshwar4321@gmail.com>
2022-04-08 01:11:48 +0530

    changes for payment in promoted student

--- routes/registerdForTest.js
+++ routes/registerdForTest.js
@@ -5,11 +5,11 @@ const { promoteStudentRank, payforPromotedstudens, removeFromRegisterd, mergedDo
 const { requireSignin, isAuth, verifySchool } = require("../controllers/auth");
 
 router.put("/registered_students/:userId", requireSignin, verifySchool, promoteStudentRank);
-router.put("/promoted_students/:userId", requireSignin, verifySchool, payforPromotedstudens);
+router.put("/pay_for_promoted_students/:userId/:promtedId", requireSignin, verifySchool, payforPromotedstudens);
 router.delete("/registered_students/remove_student/:userId/:registeredId", verifySchool, removeFromRegisterd);
-router.post("/registered/mergeDocs/:userId/:studentId/:registeredId", requireSignin, verifySchool,mergedDocForTest);
-router.post("/registered/mergeDocs/:userId", requireSignin,verifySchool, multipleDocMerge);
-router.delete("/registered/removeAll/:userId", requireSignin,verifySchool, deleteAll);
+router.post("/registered/mergeDocs/:userId/:studentId/:registeredId", requireSignin, verifySchool, mergedDocForTest);
+router.post("/registered/mergeDocs/:userId", requireSignin, verifySchool, multipleDocMerge);
+router.delete("/registered/removeAll/:userId", requireSignin, verifySchool, deleteAll);
 
 
 
