29969d4c977c0eec5172cc01180a034e77e6b2bb
diwakar17 <diwakar17@navgurukul.org>
2022-08-20 13:31:49 +0530

    apptOnCategory route correction

--- routes/appointment.js
+++ routes/appointment.js
@@ -8,7 +8,7 @@ router.post("/add_appointment/:userId", verifySchool, appointment.Create);
 router.post("/add_appointment/v2/:userId", verifySchool, upload.single('eventBanner'), appointment.apptCreate);
 router.post("/eventmanager/addguest/sendemail/:userId",verifySchool,appointment.sendEmailToGuest);
 router.get("/appointment/list_of_appointments/:userId/:dates", verifySchool, appointment.read);
-router.get("_onCategory/:userId/:page_no/:per_page/:catType", verifySchool, appointment.catRead);
+router.get("/appointment/list_of_appointments_onCategory/:userId/:page_no/:per_page/:catType", verifySchool, appointment.catRead);
 router.get("/appointment/list_of_appoinment_info/:userId/:appointId", requireSignin, appointment.appointInfo)
 router.put("/appointment/update_appointment/:userId/:appointId", requireSignin, upload.single('eventBanner'), appointment.update);
 router.put("/appointment/update_all_appointment/:userId/:oldcategoryname", requireSignin, appointment.updateAll);
