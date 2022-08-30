085177f5fb917715fda45bce4e80aa8b7e11dda0
diwakar17 <diwakar17@navgurukul.org>
2022-08-30 15:16:19 +0530

    changes for contact details in text

--- routes/text_chat_routes.js
+++ routes/text_chat_routes.js
@@ -17,7 +17,7 @@ router.post("/text-chat/add-contact/:userId", verifySchool, addTextContact);
 router.post("/text-chat/send-message/:userId", verifySchool, sendTextMessage);
 router.post("/text-chat/seen-contact-messages/:contact/:userId", verifySchool, seenContactTextMessages);
 router.post("/text-chat/pin-contact/:contact/:userId", verifySchool, pinContact);
-router.post("/text-chat/contacts-details/:userId", verifySchool, getTextContactsDetails);
+router.get("/text-chat/contacts-details/:userId", verifySchool, getTextContactsDetails);
 router.get("/text-chat/get-contacts/:userId", verifySchool, getTextContacts);
 router.get("/text-chat/get-messages/:userId/:uid", verifySchool, getTextMessages);
 router.post("/text-chat/listen-message/:twilio", listenIncomingSMS);
